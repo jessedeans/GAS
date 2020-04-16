5a862633e2289f24dc313a2ebfb3ff5150dc3514
Jesse Dean Shumway <JD@JD-MacBook-Pro.local>
2020-04-16 14:15:35 -0500

    Update GAS.py

--- GAS.py
+++ GAS.py
@@ -2,15 +2,19 @@
 """
 
 
-Code written for GAS to combine staffing database containing cost data with 
-collections database containing revenue data
+Code written for medical client to combine staffing database with collections
+database to ulitmately determine optimum number of shifts. The companys largest 
+cost is staffing a hostpical shift, and the Cclient wants to know how 
+profitable each shift is. The collections database contains the of cases and 
+revenue of each provider. By combining these two databases the client will have 
+a better understanding of how many shifts are profitable. 
 
 Before running the code, 
-    1. remve all instances of DR from staffing databse to ensure shift matches 
+    1. Remve all instances of 'DR' from staffing databse to ensure shift matches 
        to propper case lines
-    2. Format Dates in both databases as mm/dd/yy 
+    2. Format dates in both databases as mm/dd/yy 
     3. Remove row 1 in collections
-    4. Delete trailing data in last rows of collections
+    4. Delete export metadata in last rows of collections
 
 
 """
@@ -18,118 +22,149 @@ Before running the code,
 import pandas as pd
 import numpy as np
 
-#%% Import Data
+#%% Import Raw Data
 #import data containing Shifts (cost data)
-staff = pd.read_csv('MostRecentData/SHC 12 Mo Staffing.csv', encoding="ISO-8859-1")
+staff = pd.read_csv('MostRecentData/SHC 12 Mo Staffing.csv', 
+                    encoding="ISO-8859-1")
 #import data containing cases (revenue data)
-collections = pd.read_csv('MostRecentData/SHC Collections by DOS_04-14-2020.csv', encoding="ISO-8859-1")
+collections = pd.read_csv('MostRecentData/SHC Collections by DOS_04-14-2020.csv',
+                          encoding="ISO-8859-1")
 
 #%%Clean Data
 
 #delete unnecessary columns from shift data
 staff = staff[['User','Date','Shift']]
-staff = staff.dropna(axis = 'rows')
-collections = collections.dropna(axis = 'rows')
+#delete unnessary columns from collections data
+collections = collections.drop(columns = 'ASA')
+#Delete empty rows 
+staff = staff.dropna(axis = 'rows', how = 'all')
+collections = collections.dropna(axis = 'rows', how = 'all')
 
+#%% Create join column by combing names and date strings 
 
-#%% Create Join column by combing names and dates
+#Each Database contains a provider and a date, albeit in different formats
+#Databses will be joined on a unique provider date combination
 
-#Seperate provider first and last names
+#Seperate provider first and last names and standardize case
 staff[['Last Name', 'First Name']] = staff['User'].str.split(',', expand = True)
 staff['Last Name'] = staff['Last Name'].str.upper()
-collections[['Last Name', 'First Name']] = collections['Rendering Provider'].str.split(',', expand = True)
+collections[['Last Name', 'First Name']
+            ] = collections['Rendering Provider'].str.split(',', expand = True)
 
 #Strip names of extra spaces
 collections['First Name'] = collections['First Name'].str.strip()
 staff['First Name'] = staff['First Name'].str.strip()
 
-#Change Ryan G so not confused from Rayn C
+#Two providers with last name Ryan, add first initial to create unique ID
+# Ryan G
 staff.loc[staff['User'] == 'Ryan, G.', 'Last Name'] = 'RYANG'
 collections.loc[collections['Rendering Provider'] == 
                 'RYAN, WILLIAM G', 'Last Name'] = 'RYANG'
-
-#Change Ryan C so not confused with Ryan G
+# Ryan C
 staff.loc[staff['User'] == 'Ryan, C.', 'Last Name'] = 'RYANC'
 collections.loc[collections['Rendering Provider'] == 
                 'RYAN, CRAIG', 'Last Name'] = 'RYANC'
 
-# Create Column to join databases 
-collections['LookUp'] = collections['Last Name'] + '_' + collections['Date of Service - Case']
+# Create column to join databases 
+collections['LookUp'] = collections['Last Name'
+                                    ] + '_' + collections['Date of Service - Case']
 staff['LookUp'] = staff['Last Name'] + '_' + staff['Date']
 
-#%% Format Date Columns
+#%% Format Date Columns and add day of week
 
 #format Date Columns
 staff['Date'] = pd.to_datetime(staff['Date'])
-collections['Date of Service - Case'] = pd.to_datetime(collections['Date of Service - Case'])
+collections['Date of Service - Case'] = pd.to_datetime(
+    collections['Date of Service - Case'])
 
 #add Day of Week Column
 staff['Staff_Day'] = staff['Date'].dt.dayofweek
 collections['Case_Day'] = collections['Date of Service - Case'].dt.day_name()
 
 #%% Clean shift names with formatted dates
+
+#Weekend call shift not standarized in databses sometimes Call sometimes WE CALL
+#Weekday calls shfits are not assigned revenue so need to differentiate
+
 #Change MV Call on weekends to MV WE Call 1
-staff.loc[(staff['Shift'] == 'MV Call 1') & (staff['Staff_Day'] == 5) , 'Shift'] = 'MV WE Call 1' 
-staff.loc[(staff['Shift'] == 'MV Call 1') & (staff['Staff_Day'] == 6) , 'Shift'] = 'MV WE Call 1'
+staff.loc[(staff['Shift'] == 'MV Call 1') & (staff['Staff_Day'] == 5) , 
+          'Shift'] = 'MV WE Call 1' 
+staff.loc[(staff['Shift'] == 'MV Call 1') & (staff['Staff_Day'] == 6) , 
+          'Shift'] = 'MV WE Call 1'
 
 
 #%% Filter out Call shifts - Remove Call shifts and add back in Weekend call shifts
 
-#Remove all call shifts from staff
-staffShiftFiltered = staff[staff.Shift.str.contains('Call') == False]
-#find weekend call shifts
-wkndCallShifts = staff[staff.Shift.str.contains('Call WE | WE Call') == True]
-#add weekend call shifts back to shifts
-staffShiftFiltered = staffShiftFiltered.append(wkndCallShifts)
+#Providers have a shift for busniess hour cases and a call shift of after hours work.
+#Revenue should be assinged to the normal shift, not the call shift. 
+#On weekends there are only call shifts. 
+#Revenue should be assigned to weekend call shifts 
 
+#Remove all call shifts from staff
+staff_shift_Filtered = staff[staff.Shift.str.contains('Call') == False]
+#find weekend call shifts
+wknd_call_shifts = staff[staff.Shift.str.contains('Call WE | WE Call') == True]
+#add weekend call shifts back to shifts
+staff_shift_Filtered = staff_shift_Filtered.append(wknd_call_shifts)
 #store Exluded Call Shifts
-staffExcludedShifts = staff[staff.Shift.str.contains('Call') == True]
-staffExcludedShifts = staffExcludedShifts.drop(staff[staff.Shift.str.contains(
+staff_excluded_shifts = staff[staff.Shift.str.contains('Call') == True]
+staff_excluded_shifts = staff_excluded_shifts.drop(staff[staff.Shift.str.contains(
     'Call WE | WE Call') == True].index)
 
 #%% Find Duplicated Shifts
 
-duplicatedShifts = staffShiftFiltered[staffShiftFiltered.duplicated(['LookUp'], keep = False)]
-duplicatedShifts = duplicatedShifts.assign(collections=duplicatedShifts.LookUp.isin(collections.LookUp).astype(str))
-duplicatedShifts.rename(columns={'collections':'Provider has Cases on Date'}, inplace = True)
+duplicatedShifts = staff_shift_Filtered[
+    staff_shift_Filtered.duplicated(['LookUp'], keep = False)]
+duplicatedShifts = duplicatedShifts.assign(
+    collections=duplicatedShifts.LookUp.isin(collections.LookUp).astype(str))
+duplicatedShifts.rename(
+    columns={'collections':'Provider has Cases on Date'}, inplace = True)
 duplicatedShifts.drop(columns = 'LookUp', inplace = True)
+print('There are ' + str(len(duplicatedShifts)/2) + ' dupliated shifts in first pass')
+
 #%% Join Databases
 
 #Select staff Columns to join with collections
-staffClean = staffShiftFiltered[['User','Date','Shift','LookUp','Staff_Day']]
-
+staffClean = staff_shift_Filtered[['User','Date','Shift','LookUp','Staff_Day']]
 #Join shift (cost) and cases (revenue) databases
 join = pd.merge(collections,staffClean,on='LookUp', how = 'outer')
-#join['Date'] = pd.to_datetime(join['Date'])
 
 #%%identify cases without a shift and shifts without cases
-nanCount = join.isna().sum()
+#nanCount = join.isna().sum()
 empty_shifts = join[join['CPT'].isnull()]
 empty_cases = join[join['Shift'].isnull()]
+print ('There are ' + str(len(empty_shifts)) + ' empty shifts in first pass')
+print ('There are ' + str(len(empty_cases)) + ' empty cases in first pass')
 
 #%%remove rows with incmomplete data
-joinCleaned = join.dropna(axis ='rows')
+join_cleaned = join.dropna(axis ='rows')
 
 #%% Repeat Join for call shfits 
 
+#There may be instances where a provider only had a call shift on a day
+#Revenue should be assigned to these call shifts
+
 #Join empty shifts and excluded shifts
 empty_shifts = empty_shifts.dropna(axis = 'columns', how = 'all')
-empty_shifts = empty_shifts.append(staffExcludedShifts)
-#drop na from cases
+empty_shifts = empty_shifts.append(staff_excluded_shifts)
+#drop na from cases for cleaner join
 empty_cases = empty_cases.dropna(axis = 'columns', how = 'all')
 #Select staff Columns to join with collections
 empty_shifts = empty_shifts[['User','Date','Shift','LookUp','Staff_Day']]
 empty_join = pd.merge(empty_shifts,empty_cases,on='LookUp', how = 'outer')
 empty_join_cleaned = empty_join.dropna(axis ='rows')
 #append call cases to join
-joinCleaned = joinCleaned.append(empty_join_cleaned)
+join_cleaned = join_cleaned.append(empty_join_cleaned)
 #identify cases without a shift and shifts without cases
 empty_shifts_all = empty_join[empty_join['CPT'].isnull()]
 empty_casees_all = empty_join[empty_join['Shift'].isnull()]
+print ('There are ' + str(len(empty_shifts_all)) + ' empty shifts in second pass')
+print ('There are ' + str(len(empty_casees_all)) + ' empty cases in second pass')
 
-#%% Add fill in missing data and add back in empty cases and shifts
-#Add facility to empty shifts
-#emptyShifts['Facility'] = ''
+
+#%% Add unmatched data back
+
+#Facility can be assumed from shift name.
 empty_shifts_all.loc[empty_shifts_all['Shift'].str.contains('MV'), 
                        ['Facility']] = 'MOUNTAIN VISTA-MAIN'
 empty_shifts_all.loc[empty_shifts_all['Shift'].str.contains('TSL'), 
@@ -142,25 +177,37 @@ empty_shifts_all.loc[empty_shifts_all['Shift'].str.contains('Luke\'s'),
                        ['Facility']] = 'TEMPE ST LUKES  MEDICAL CENTER'
 empty_shifts_all.loc[empty_shifts_all['Shift'].str.contains('Physician Call'), 
                        ['Facility']] = 'MOUNTAIN VISTA-MAIN'
-#Add Shift to empty cases
+#Create unkonwn shift for cases without a shift
 empty_cases['Shift'] = 'Unknown Shift'
+#Add unmatched data back
+join_all =join_cleaned.append([empty_shifts_all,empty_casees_all])
 
-join_all =joinCleaned.append([empty_shifts_all,empty_casees_all])
-
-#%% Add missings dates to new columns
+#%% Create new date column with date for every row
 join_all['Final_Date'] = np.NaN
 join_all['Final_Date'] = join_all['Date'].fillna(
     join_all['Date']).fillna(join_all['Date of Service - Case'])
 
-#%% Sum payment on facility and shift
-# expectedShift = joinCleaned.groupby(['Facility','Shift'])['Expected'].sum()
-# paymentsShift = joinCleaned.groupby(['Facility','Shift'])['Payments - All'].sum()
-# balanceShift = joinCleaned.groupby(['Facility','Shift'])['Balance'].sum()
-# shiftCount = joinCleaned.groupby(['Facility','Shift'])['Shift'].count()
-#
+#%% Create Outputs
 
-#Group by year, month, facility. Aggreate cases and financials
-output = join_all.groupby([join_all['Final_Date'].dt.year.rename('year'), 
+#Create monthly totals dataframe. 
+#Group by year month and facility. Aggregate shifts and financials
+facility_group = join_all.groupby([join_all['Final_Date'].dt.year.rename('year'), 
+                              join_all['Final_Date'].dt.month.rename('month'), 
+                              'Facility'] 
+                             ).agg({
+                                  'LookUp' : lambda x: x.nunique(),
+                                  'Expected':sum,
+                                  'Payments - All':sum,
+                                  'Balance':sum })
+
+facility_group.index = [facility_group.index.get_level_values(0), 
+                        facility_group.index.get_level_values(1),
+                        facility_group.index.get_level_values(2),
+                        ['Total'] * len(facility_group)]
+                                                                                               
+#Create shift level dataframe
+#Group by year, month, facility, and shift. Aggreate shifts and financials
+shift_group = join_all.groupby([join_all['Final_Date'].dt.year.rename('year'), 
                               join_all['Final_Date'].dt.month.rename('month'), 
                               'Facility', 
                               'Shift']
@@ -169,13 +216,15 @@ output = join_all.groupby([join_all['Final_Date'].dt.year.rename('year'),
                                   'Expected':sum,
                                   'Payments - All':sum,
                                   'Balance':sum })
-# #%%
-# #Export Data
-# emptyShifts.loc[:,'User':'Shift'].to_csv('Output/Empty_Shifts.csv',index = False)
-# emptyCases.loc[:,'Case ID':'Balance'].to_csv('Output/Cases_WithOut_Shifts.csv', index = False)
-# duplicatedShifts.to_csv('Output/Duplicated_Shifts.csv', index = False)
-# output.to_csv('Output/Sum_By_Shift_By_Facility.csv')
 
-# # expectedShift.to_csv('Excpected.csv')
-# # paymentsShift.to_csv('Payment.csv')
-# # balanceShift.to_csv('Balance.csv')
+#concatenate dataframes to have shift level detail with monthly totals 
+output = pd.concat([facility_group, shift_group]).sort_index(level=[0,1,2])
+
+#rename columns for client
+output = output.rename(columns = {'Index': 'Shift', 'LookUp':'Shift_Count'})
+#%% Export Data
+
+#output.to_csv('Output/output.csv')
+
+
+
